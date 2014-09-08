995d73a276025316091d3f896656d686f4a7d757
kobbesun <riyutian@gmail.com>
2014-09-08 19:55:37 +0800

    plot 1

---
+++ plot1.R
@@ -0,0 +1,30 @@
+# import data.table library
+library(data.table)
+
+# get column names
+dt_uci_5rows <- fread("../household_power_consumption.txt", sep="auto", 
+                      nrows=5, header=TRUE)
+print(dt_uci_5rows)
+
+# get target records first position and length
+dt_uci_date <- fread("../household_power_consumption.txt", sep="auto", nrows=-1, 
+                     header=TRUE, na.strings="?", select=1)
+dt_uci_date[, index:=.I]
+row_skip <- dt_uci_date[dt_uci_date$Date=="1/2/2007" | 
+                        dt_uci_date$Date=="2/2/2007", ][[2]][[1]]
+count_rows <- dt_uci_date[dt_uci_date$Date=="1/2/2007" | 
+                          dt_uci_date$Date=="2/2/2007", .N] 
+
+# obtain target records
+dt_uci_target <- fread("../household_power_consumption.txt", sep="auto",
+                       nrows=count_rows, header=FALSE, skip=row_skip, 
+                       na.strings="?")
+
+# assign column names
+setnames(dt_uci_target, names(dt_uci_5rows))
+
+# Plot 1
+hist(dt_uci_target$Global_active_power, 
+     freq = TRUE, col="red", 
+     main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
